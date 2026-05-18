9dea60fdd0901c053484a598b812a18fe061451f
Luan Fabrício <luanlimaleite@hotmail.com>
2026-05-17 23:15:40 -0300

    refactor: removes unused code and renames container module alias

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/moby/moby/client"
 
 	"go-integration-tests/db"
-  g_container "go-integration-tests/container"
+  gContainer "go-integration-tests/container"
 );
 
 func main() {
@@ -22,26 +20,23 @@ func main() {
 	}
 	defer apiClient.Close()
 
-	// showContainers(apiClient)
-	// showImages(apiClient)
-
-  containerName := "auto-integrated-tests-database"
-  err = g_container.StartPostgresContainer(
+  const containerName string = "auto-integrated-tests-database"
+  err = gContainer.StartPostgresContainer(
     apiClient,
     containerName,
   )
 
-  fmt.Printf("container: %v\n", containerName)
+  fmt.Printf("container: %s\n", containerName)
 
   if err != nil {
     panic(err)
   }
 
-  time.Sleep(g_container.ContainerSetupDelay)
+  time.Sleep(gContainer.ContainerSetupDelay)
 
   runSetup()
 
-  err = g_container.StopAndKillContainer(apiClient, containerName)
+  err = gContainer.StopAndKillContainer(apiClient, containerName)
 
   if err != nil {
     panic(err)
@@ -67,34 +62,4 @@ func runSetup() {
 
   db.SetupDB(dbConnection)
 
-}
-
-func showContainers(apiClient client.APIClient) {
-	result, err := apiClient.ContainerList(
-		context.Background(),
-		client.ContainerListOptions{All: true},
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, ctr := range result.Items {
-		name := strings.Join(ctr.Names, ", ")
-		fmt.Printf("%s | %s | %s | (status: %s)\n", ctr.ID, name, ctr.Image, ctr.Status)
-	}
-}
-
-func showImages(apiClient client.APIClient) {
-	result, err := apiClient.ImageList(
-		context.Background(),
-		client.ImageListOptions{},
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, img := range result.Items {
-		tags := strings.Join(img.RepoTags, ", ")
-		fmt.Printf("%s | %s | %d\n", img.ID, tags, img.Containers)
-	}
 }
